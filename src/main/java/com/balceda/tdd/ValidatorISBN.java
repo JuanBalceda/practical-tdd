787509e8e40a245c6e56472a65262eb9a06c2220
Juan Balceda <juanbalceda@gmail.com>
2019-01-27 01:29:56 +0100

    Chapter 04 - Validation completed

--- src/main/java/com/balceda/tdd/ValidatorISBN.java
+++ src/main/java/com/balceda/tdd/ValidatorISBN.java
@@ -1,21 +1,47 @@
 package com.balceda.tdd;
 
-import java.lang.reflect.Array;
-
 public class ValidatorISBN {
-    public boolean checkISBN(String isbn) {
-        if (isbn.length() < 10){
-            throw new NumberFormatException();
-        }
-        int total = 0;
-        for (int i = 0; i < 10; i++) {
-            total += isbn.charAt(i) * (10 - i);
-        }
+	public boolean checkISBN(String isbn) {
+		if (isbn.length() == 13) {
+			int total = 0;
+			for (int i = 0; i < 13; i++) {
+				if (i % 2 == 0) {
+					total += Character.getNumericValue(isbn.charAt(i));
+				} else {
+					total += Character.getNumericValue(isbn.charAt(i)) * 3;
+				}
+			}
 
-        if (total % 11 == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+			if (total % 10 == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+
+			if (isbn.length() < 10) {
+				throw new NumberFormatException("ISBN numbers must be 10 digits long");
+			}
+
+			int total = 0;
+			for (int i = 0; i < 10; i++) {
+				if (!Character.isDigit(isbn.charAt(i))) {
+					if (i == 9 && isbn.charAt(i) == 'X') {
+						// This is Ok
+						total += 10;
+					} else {
+						throw new NumberFormatException("ISBN numbers must be only numerics digits");
+					}
+				} else {
+					total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
+				}
+			}
+
+			if (total % 11 == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
 }
--- src/test/java/com/balceda/tdd/AppTest.java
+++ src/test/java/com/balceda/tdd/AppTest.java
@@ -6,27 +6,58 @@ import static org.junit.Assert.*;
 
 public class AppTest {
 
-    @Test
-    public void shouldBeValidISBN() {
-        ValidatorISBN validator = new ValidatorISBN();
-        boolean result;
-        result = validator.checkISBN("1484233867");
-        assertTrue("First result", result);
+	@Test
+	public void checkBeValid10ISBN() {
+		ValidatorISBN validator = new ValidatorISBN();
+		boolean result;
+		result = validator.checkISBN("1484233867");
+		assertTrue("First result", result);
 
-        result = validator.checkISBN("1788293037");
-        assertTrue("Second result", result);
-    }
+		result = validator.checkISBN("1788293037");
+		assertTrue("Second result", result);
+	}
 
-    @Test
-    public void shouldNotBeValidISBN() {
-        ValidatorISBN validator = new ValidatorISBN();
-        boolean result = validator.checkISBN("1484933837");
-        assertFalse(result);
-    }
+	@Test
+	public void checkInvalid10ISBN() {
+		ValidatorISBN validator = new ValidatorISBN();
+		boolean result = validator.checkISBN("1484933837");
+		assertFalse(result);
+	}
 
-    @Test(expected = NumberFormatException.class)
-    public void nineDigitsISBNAreNotAllowed() {
-        ValidatorISBN validator = new ValidatorISBN();
-        boolean result = validator.checkISBN("123456789");
-    }
+	@Test
+	public void checkBeValid13DigitsISBN() {
+		ValidatorISBN validator = new ValidatorISBN();
+		boolean result;
+		result = validator.checkISBN("9781853260087");
+		assertTrue("First result", result);
+
+		result = validator.checkISBN("9781853267338");
+		assertTrue("Second result", result);
+	}
+
+	@Test
+	public void checkInvalid13ISBN() {
+		ValidatorISBN validator = new ValidatorISBN();
+		boolean result = validator.checkISBN("9781853267339");
+		assertFalse(result);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void nineDigitsISBNAreNotAllowed() {
+		ValidatorISBN validator = new ValidatorISBN();
+		validator.checkISBN("123456789");
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void notNumericISBNAreNotAllowed() {
+		ValidatorISBN validator = new ValidatorISBN();
+		validator.checkISBN("helloworld");
+	}
+
+	@Test
+	public void tenISBNNumbersEndingInAnXAreValid() {
+		ValidatorISBN validator = new ValidatorISBN();
+		boolean result = validator.checkISBN("012000030X");
+		assertTrue(result);
+	}
 }
